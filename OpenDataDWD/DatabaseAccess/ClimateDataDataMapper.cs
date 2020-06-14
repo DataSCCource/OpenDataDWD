86bfb0b3a277d2e9210b78fe6fbadfce43f8428a
DennisW <dontspamme@gmx.org>
2020-06-14 12:45:39 +0200

    Add ClimateDataDataMapper and comment StationDataMapper
    Change-Id: I7de3a106c3fc727453cc33b369dea895c2bf91f5

---
+++ OpenDataDWD/DatabaseAccess/ClimateDataDataMapper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace DatabaseAccess
+{
+    class ClimateDataDataMapper : DataMapper
+    {
+        public static readonly int STAT = 0;  // Stationsnummer
+        public static readonly int DATUM = 1; // Datum der Messung
+        public static readonly int PM = 2;    // Tagesmittel des Luftdruckes in Stationshoehe 
+        public static readonly int TXK = 3;   // Tagesmaximum der Temperatur der Luft in 2m Hoehe 
+        public static readonly int TNK = 4;   // Tagesminimum der Temperatur der Luft in 2m Hoehe 
+        public static readonly int UPM = 5;   // Tagesmittel der relativen Feuchte 
+        public static readonly int FMK = 6;   // Tagesmittel der Windstaerke 
+        public static readonly int SDK = 7;   // Tagessumme der Sonnenscheindauer 
+
+        /// <summary>
+        /// Get List of DataFieldDefinitions for climatedata in KL- and KX-format
+        /// </summary>
+        /// <returns></returns>
+        protected override List<DataFieldDefinition> GetMapperList()
+        {
+            return new List<DataFieldDefinition>
+                {
+                    new DataFieldDefinition { Start=2,   Length=5 }, // STAT
+                    new DataFieldDefinition { Start=7,   Length=8 }, // DATUM
+                    new DataFieldDefinition { Start=37,  Length=5 }, // PM
+                    new DataFieldDefinition { Start=43,  Length=4 }, // TXK
+                    new DataFieldDefinition { Start=48,  Length=4 }, // TNK
+                    new DataFieldDefinition { Start=125, Length=3 }, // UPM
+                    new DataFieldDefinition { Start=160, Length=3 }, // FMK
+                    new DataFieldDefinition { Start=195, Length=3 }, // SDK
+                };
+        }
+    }
+}
--- OpenDataDWD/DatabaseAccess/StationDataMapper.cs
+++ OpenDataDWD/DatabaseAccess/StationDataMapper.cs
@@ -1,24 +1,18 @@
-﻿using DatabaseAccess;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace DatabaseAccess
 {
-
     class StationDataMapper : DataMapper
     {
-        public static readonly int ST_KE = 0;
-        public static readonly int ST_ID = 1;
-        public static readonly int VON = 2;
-        public static readonly int BIS = 3;
-        public static readonly int ST_HOEHE = 4;
-        public static readonly int GEO_BREITE = 5;
-        public static readonly int GEO_LAENGE = 6;
-        public static readonly int STATIONSNAME = 7;
-        public static readonly int BUNDESLANDNAME = 8;
+        public static readonly int ST_KE = 0;          // Kennung
+        public static readonly int ST_ID = 1;          // ID
+        public static readonly int VON = 2;            // Daten von
+        public static readonly int BIS = 3;            // Daten bis
+        public static readonly int ST_HOEHE = 4;       // Stationshoehe
+        public static readonly int GEO_BREITE = 5;     // Latitude
+        public static readonly int GEO_LAENGE = 6;     // Longitude
+        public static readonly int STATIONSNAME = 7;   // Stationsname
+        public static readonly int BUNDESLANDNAME = 8; // Station in Bundesland
 
         protected override List<DataFieldDefinition> GetMapperList()
         {
@@ -35,6 +29,5 @@ namespace DatabaseAccess
                     new DataFieldDefinition { Start=88, Length=25 }, // Bundeslandname
                 };
         }
-
     }
 }
